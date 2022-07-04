5251fbfadc60e25dbb4fce07ff53de3a095cd442
unknown <93chungil@gmail.com>
2022-07-04 11:09:05 -0700

    static events page

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const contact_form = document.getElementById("contact-form");
+const reader = new FileReader();
+
 (function () {
     [...document.querySelectorAll(".control")].forEach(button => {
         button.addEventListener("click", function() {
@@ -11,3 +14,15 @@
         document.body.classList.toggle("light-mode");
     })
 })();
+
+function go_contact() {
+    document.querySelector(".active-btn").classList.remove("active-btn");
+    document.querySelector('[data-id="contact"]').classList.add("active-btn");
+    document.querySelector(".active").classList.remove("active");
+    document.getElementById("contact").classList.add("active");
+};
+
+function send_email() {
+    contact_form.submit();
+    contact_form.reset();
+};
